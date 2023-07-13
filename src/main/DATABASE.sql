c74e85540999e9d027b9360b7bf5e7241187b178
hery00 <handrianimanana@gmail.com>
2023-07-13 23:51:29 +0300

    Update DATABASE.sql

--- src/main/DATABASE.sql
+++ src/main/DATABASE.sql
@@ -19,6 +19,8 @@ INSERT INTO Avion VALUES (default,20, 100);
 INSERT INTO Avion VALUES (default,10, 150);
 INSERT INTO Avion VALUES (default,30, 80);
 
+
+
 create table Vol (
     Id_Vol serial primary key,
     Numero_Vol int,
@@ -27,7 +29,31 @@ create table Vol (
     Destination VARCHAR(50),
     Date_Vol Timestamp,
     Prix int,
+    date_soumission TIMESTAMP,
+	est_valide BOOLEAN DEFAULT false,
+	date_validation TIMESTAMP,
     foreign key(Id_Avion) references Avion(Id_Avion)
+);
+
+INSERT INTO Vol (Numero_Vol, Id_Avion, Source, Destination, Date_Vol, Prix, date_soumission)
+VALUES (12345, 1, 'Paris', 'New York', '2023-07-15 10:00:00', 1000, current_timestamp);
+INSERT INTO Vol (Numero_Vol, Id_Avion, Source, Destination, Date_Vol, Prix, date_soumission)
+VALUES (67890, 2, 'Londres', 'Dubai', '2023-07-20 15:30:00', 1500, current_timestamp);
+INSERT INTO Vol (Numero_Vol, Id_Avion, Source, Destination, Date_Vol, Prix, date_soumission)
+VALUES (24680, 2, 'Tokyo', 'Sydney', '2023-07-25 08:45:00', 2000, current_timestamp);
+INSERT INTO Vol (Numero_Vol, Id_Avion, Source, Destination, Date_Vol, Prix, date_soumission)
+VALUES (13579, 3, 'Los Angeles', 'Lima', '2023-07-30 12:15:00', 1200, current_timestamp);
+INSERT INTO Vol (Numero_Vol, Id_Avion, Source, Destination, Date_Vol, Prix, date_soumission)
+VALUES (98765, 1, 'Rome', 'Berlin', '2023-08-05 09:30:00', 800, current_timestamp);
+
+
+CREATE TABLE vol_attente_validation(
+  id SERIAL PRIMARY KEY,
+  employe_id INT,
+  vol_details VARCHAR(200),
+  date_soumission TIMESTAMP,
+  est_valide BOOLEAN DEFAULT false,
+  date_validation TIMESTAMP
 );
 ALTER TABLE Vol ADD Validations BOOLEAN;
 INSERT INTO Vol VALUES (default, 1000, 1, 'Paris', 'New York', '2023-07-05 14:30:00', 1500, true);
